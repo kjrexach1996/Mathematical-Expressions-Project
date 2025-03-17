5dfc26c6a93a76866b87bfe80bcaa9e4e6f8f1f1
Kevin Rexach <kaiorex@hotmail.com>
2025-03-17 16:16:33 -0400

    Part 2 of Mathematical Expressions Project.

--- Mathematical Expressions Project.cpp
+++ Mathematical Expressions Project.cpp
@@ -1,8 +1,8 @@
-// Mathematical Expressions Project.cpp : The purpose of Part 1 this project is to convert
-// an infix mathematical expression into a postfix expression using the functions convertInfixToPostfix()
-// and precedes(). Any correctly written expression should be handled regardless of proper spacing.
-// The function should account for exceptions and display error messages when encountering expressions
-// that included unpaired parentheses and improper formatting of operators and operands.
+// Mathematical Expressions Project.cpp : The purpose of Part 2 this project is to evaluate a
+// postfix expression. The function evalPostfix() will use a stack to push the integers encountered
+// in the postfix expression, then perform an operation on the top two operands when the first operation
+// is encountered in the expression. This result will then be pushed to the stack. This loop will repeat 
+// until there are no more characters in the expression to process, and the final result is returned.
 
 #include <iostream>
 #include <iomanip>
@@ -12,6 +12,21 @@
 
 int main()
 {
+    //Testing for default constructor; default parameters set to "2 2 +"
+    PostFixEvaluator myExpression;
+    cout << "Current postfix expression: " << myExpression.getPostfix() << endl;
+    myExpression.evalPostfix();
+    cout << "Final postfix result: " << myExpression.getResult() << endl;
+    cout << endl;
+
+    //Testing functions using user-defined parameters
+    myExpression.setPostfix("25 43 17 + *");
+    cout << "Current postfix expression: " << myExpression.getPostfix() << endl;
+    myExpression.evalPostfix();
+    cout << "Final postfix result: " << myExpression.getResult() << endl;
+    cout << endl;
+
+    //Reading and processing a file containing postfix expressions
     string postfix;
     ifstream input;
     input.open("RpnData.txt");
@@ -19,12 +34,11 @@ int main()
     while (getline(input, postfix))
     {
         try {
-            //Testing for default constructor; default parameters set to "22+"
-            PostFixEvaluator myExpression(postfix);
+
+            myExpression.setPostfix(postfix);
             cout << "Current postfix expression: " << myExpression.getPostfix() << endl;
             myExpression.evalPostfix();
-            cout << "Final postfix result: " << fixed << setprecision(2) << myExpression.getResult() << endl;
-            cout.unsetf(ios::fixed);
+            cout << "Final postfix result: " << myExpression.getResult() << fixed << setprecision(5) << endl;
         }
         catch (runtime_error& e)
         {
@@ -32,8 +46,86 @@ int main()
         }
         cout << endl;
     }
-
     input.close();
+
+    //***EXCEPTIONS***//
+    //Insufficient operands
+    try {
+        myExpression.setPostfix("3 4 6 - * +");
+        cout << "Current postfix expression: " << myExpression.getPostfix() << endl;
+        myExpression.evalPostfix();
+        cout << "Final postfix result: " << myExpression.getResult() << fixed << setprecision(5) << endl;
+    }
+    catch (runtime_error& e)
+    {
+        cout << "Error: " << e.what() << endl;
+    }
+    cout << endl;
+
+    //Too many operands in expression
+    try {
+        myExpression.setPostfix("3 4 5 +");
+        cout << "Current postfix expression: " << myExpression.getPostfix() << endl;
+        myExpression.evalPostfix();
+        cout << "Final postfix result: " << myExpression.getResult() << fixed << setprecision(5) << endl;
+    }
+    catch (runtime_error& e)
+    {
+        cout << "Error: " << e.what() << endl;
+    }
+    cout << endl;
+
+    //Division by 0
+    try {
+        myExpression.setPostfix("5 0 /");
+        cout << "Current postfix expression: " << myExpression.getPostfix() << endl;
+        myExpression.evalPostfix();
+        cout << "Final postfix result: " << myExpression.getResult() << fixed << setprecision(5) << endl;
+    }
+    catch (runtime_error& e)
+    {
+        cout << "Error: " << e.what() << endl;
+    }
+    cout << endl;
+
+    //Invalid token found
+    try {
+        myExpression.setPostfix("8 9 6 & -");
+        cout << "Current postfix expression: " << myExpression.getPostfix() << endl;
+        myExpression.evalPostfix();
+        cout << "Final postfix result: " << myExpression.getResult() << fixed << setprecision(5) << endl;
+    }
+    catch (runtime_error& e)
+    {
+        cout << "Error: " << e.what() << endl;
+    }
+    cout << endl;
+
+    //Stack underflow
+    try {
+        myExpression.setPostfix("+ 3 4");
+        cout << "Current postfix expression: " << myExpression.getPostfix() << endl;
+        myExpression.evalPostfix();
+        cout << "Final postfix result: " << myExpression.getResult() << fixed << setprecision(5) << endl;
+    }
+    catch (runtime_error& e)
+    {
+        cout << "Error: " << e.what() << endl;
+    }
+    cout << endl;
+
+    //Improper formatting
+    try {
+        myExpression.setPostfix("3  +  5");
+        cout << "Current postfix expression: " << myExpression.getPostfix() << endl;
+        myExpression.evalPostfix();
+        cout << "Final postfix result: " << myExpression.getResult() << fixed << setprecision(5) << endl;
+    }
+    catch (runtime_error& e)
+    {
+        cout << "Error: " << e.what() << endl;
+    }
+    cout << endl;
 }
 
 
--- PostFixEvaluator.h
+++ PostFixEvaluator.h
@@ -3,7 +3,7 @@
 #pragma once
 #include <iostream>
 #include <stack>
-#include <sstream>
+#include <sstream> //For tokenizing strings in postfix expression
 #include <stdexcept>
 #include "InfixToPostfixConverter.h"
 
@@ -12,41 +12,57 @@ using namespace std;
 class PostFixEvaluator
 {
 private:
+	//Stores postfix expression
 	string postfix;
+
 	//Result of evaluation postfix expression
 	double postResult;
+
 public:
+
 	void evalPostfix()
 	{
-
+		//Stack to store operands
 		stack<double> st;
 
+		//istringstream object to tokenize strings
 		istringstream ss(postfix);
+
 		string token;
 
-		int index = 0;
-
+		//While tokens can be created in the expression
 		while (ss >> token)
 		{
+			//The first character of the token is an integer. If the expression follows proper formatting rules,
+			//this will account for any single- and multi-digit integers.
 			if (isdigit(token[0]))
 			{
+				//Converts the integer token from a string to a double
 				double number = stod(token);
+
+				//Stored in stack
 				st.push(number);
 			}
 				
+			//The token is an allowed operator
 			else if (token[0] == '+' || token[0] == '-' || token[0] == '*' || token[0] == '/')
 			{
+				//Exceptions handles stack underflow
+				if (st.empty())
+					throw runtime_error("Stack is empty!");
+
+				//Exception handles when there are too few operands to perform an operation
 				if (st.size() < 2)
 					throw runtime_error("Insufficient operands to perform operation.");
-
-				if (token[0] == postfix[0])
-					throw runtime_error("Operator appears before operand");
 					
+				//Takes top two operands in the stack to perform operation. Because of the order they were added to the stack,
+				//the top operand must be the second in the expression, while the next top the first.
 				double operand2 = st.top();
 				st.pop();
 				double operand1 = st.top();
 				st.pop();
 
+				//Performs operation depending on operator encountered
 				if (token[0] == '+')
 					st.push(operand1 + operand2);
 				else if (token[0] == '-')
@@ -55,27 +71,32 @@ public:
 					st.push(operand1 * operand2);
 				else if (token[0] == '/')
 				{
+					//Exception handles division by 0
 					if (operand2 == 0)
 						throw runtime_error("Cannot divide by 0.");
 						
 					st.push(operand1 / operand2);
 				}
 			}
+			//Exception handles invalid tokens
 			else
 				throw runtime_error("Invalid token encountered in postfix expression.");
 		}
+		//Exception handles additional operands that have not had an operation performed on.
 		if (st.size() > 1)
 			throw runtime_error("Too many operands left in the stack.");
 
+		//Stores final result
 		postResult = st.top();
 	}
 
+	
 	void setPostfix(string expr)
 	{
 		postfix = expr;
 	}
 
-	string getPostfix()
+	string getPostfix() const
 	{
 		return postfix;
 	}
@@ -85,6 +106,6 @@ public:
 		return postResult;
 	}
 
-	PostFixEvaluator(string postfix = "22+", double postResult = 4)
+	PostFixEvaluator(string postfix = "2 2 +", double postResult = 4)
 		: postfix(postfix), postResult(postResult) {}
 };
